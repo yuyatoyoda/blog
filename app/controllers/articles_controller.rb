ec11723fede83499a3b2b7e845bcb3857df7d2e6
Yuya Toyoda <yuya.toyoda@gmail.com>
2015-05-11 16:57:08 +0900

    articles_controller DRY化

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,15 +18,12 @@ class ArticlesController < ApplicationController
  end
 
  def show
-   @article = Article.find(params[:id])
  end
 
  def edit
-   @article = Article.find(params[:id])
  end
 
  def update
-   @article = Article.find(params[:id])
    if @article.update(article_params)
      redirect_to articles_path
    else
@@ -35,7 +32,6 @@ class ArticlesController < ApplicationController
  end
 
  def destroy
-   @article = Article.find(params[:id])
    @article.destroy
    redirect_to articles_path
  end
