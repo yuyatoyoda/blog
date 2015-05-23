3a10fefd90e37a2282f8350a2cfb9a333e81af64
Yuya Toyoda <yuya.toyoda@gmail.com>
2015-05-23 16:31:42 +0900

    development.rbなくなってる

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   has_many :articles, dependent: :destroy
